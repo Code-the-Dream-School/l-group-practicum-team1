94efdf6e75b819d9dadcad36107a22be259879d1
Seth <samaritanbrotherseth@gmail.com>
2026-05-01 21:48:58 -0400

    Getting psql connected to the neon test string to test end points for the user

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,22 +1,40 @@
+require("dotenv").config()
+const bcrypt = require('bcryptjs')
+const {statusCodes} = require('http-status-codes')
+const { userSchema}  = require('../../validations/userValidation.js')
+// const { PrismaClient } = require('@prisma/client')
+const prisma = require('../prisma.js')
+
+
+
+
+
 
 
 
 
 //Register
-async const register = (req, res) => {
-    res.json({
-        first_name: 'Seth', 
-        last_name: 'Johnson',
-        email: 'johnson@gmail.com',
-        password: 'Johnny AppleSeed',
-        created_at: new Date()
-    })
+const register = async (req, res) => {
+    if (!req.body.username || !req.body.email || !req.body.password){
+        res.status(statusCodes.BAD_REQUEST)
+    }
+    
+    const {error, value} = userSchema.validate(req.body) //validating the request body received from the user. 
+
+    const hashedpassword = await bcrypt.hash(password, 10)
+
+
+
+
+   
+
+
 }
 
 
 
 //Login ---> awaiting Prisma and data base string 
-const login = (req, res) => {
+const login = async (req, res) => {
     res.json({ message: 'This will be the login '})
 }
 
@@ -24,7 +42,7 @@ const login = (req, res) => {
 
 
 //Logout
-const logout = (req, res) => {
+const logout = async (req, res) => {
     res.end('user has logged out')
 }
 
---
+++ backend/validations/userValidation.js
@@ -0,0 +1,20 @@
+const Joi = require('joi'); //user Validation
+
+
+const userSchema = Joi.object({
+    email: Joi.string().trim().lowercase().email().required(),
+    username: Joi.string().trim().min(3).max(30).required(),
+    password: Joi.string()
+    .trim()
+    .min(8)
+    .max(30)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).+$/)
+    .required()
+    .messages({
+      "string.pattern.base":
+        "Password must be at least 8 characters long and include upper and lower case letters, a number, and a special character.",
+    }),
+});
+
+
+module.exports = userSchema
