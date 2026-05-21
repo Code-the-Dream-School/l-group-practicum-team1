17944f4a49f3b890a0000058fb6e6af685c62641
Scott Feichter <scottfeichter@gmail.com>
2026-05-20 18:20:26 -0700

    configure for dynamic loading of environments, established local db for dev, ran migration for local db, included jsonwebtoken in dependencies, deleted duplicate relation in schema

---
+++ backend/issues.md
@@ -0,0 +1,5 @@
+5.20.26 duplicate relation in the schema
+
+jsonwebtoken not found (was not in dependencies)
+
+ran - npx prisma migrate dev - no issues
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 const app = require('./src/app');
 
-
-
-
-
 const PORT = process.env.PORT || 8000;
+const NODE_ENV = process.env.NODE_ENV:
 
 app.listen(PORT, () => {
+
+  console.log(`Environment is:${NODE_ENV}`)
   console.log(`Server running on http://localhost:${PORT}`);
 });
